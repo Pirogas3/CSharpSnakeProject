73a2c68129269cffae1809fe14c6c13988858af2
Victor <Pirogas3@yandex.ru>
2026-05-19 14:14:07 +0700

    KeyboardHelper_Update

--- Input/KeyboardHelper.cs
+++ Input/KeyboardHelper.cs
@@ -1,15 +1,41 @@
-﻿using System.Runtime.InteropServices;
+﻿using SharpHook;
+using SharpHook.Data;
+using SharpHook.Native;
+using System;
 
 namespace CSharpSnakeProject.Input
 {
     public static class KeyboardHelper
     {
-        [DllImport("user32.dll")]
-        private static extern short GetAsyncKeyState(int vKey);
+        private static readonly TaskPoolGlobalHook _hook = new TaskPoolGlobalHook();
+        private static volatile bool _isShiftPressed = false;
+
+        static KeyboardHelper()
+        {
+            _hook.KeyPressed += OnKeyPressed;
+            _hook.KeyReleased += OnKeyReleased;
+            _hook.RunAsync();
+        }
+
+        private static void OnKeyPressed(object? sender, KeyboardHookEventArgs e)
+        {
+            if (e.Data.KeyCode == KeyCode.VcLeftShift || e.Data.KeyCode == KeyCode.VcRightShift)
+            {
+                _isShiftPressed = true;
+            }
+        }
+
+        private static void OnKeyReleased(object? sender, KeyboardHookEventArgs e)
+        {
+            if (e.Data.KeyCode == KeyCode.VcLeftShift || e.Data.KeyCode == KeyCode.VcRightShift)
+            {
+                _isShiftPressed = false;
+            }
+        }
 
         public static bool IsShiftHeld()
         {
-            return (GetAsyncKeyState(0x10) & 0x8000) != 0;
+            return _isShiftPressed;
         }
     }
 }
